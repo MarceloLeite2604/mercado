7638a8820182e69949810b858f7ee4ca73cf415a
MarceloLeite2604 <marceloleite2604@gmail.com>
2017-12-10 18:44:01 -0200

    Committing changes.

---
+++ mercado-new/src/main/java/org/marceloleite/mercado/nnew/TemporalTickersComparator.java
@@ -0,0 +1,14 @@
+package org.marceloleite.mercado.nnew;
+
+import java.util.Comparator;
+
+import org.marceloleite.mercado.modeler.persistence.TemporalTicker;
+
+public class TemporalTickersComparator implements Comparator<TemporalTicker> {
+
+	@Override
+	public int compare(TemporalTicker first, TemporalTicker second) {
+		return first.getFrom().compareTo(second.getFrom());
+	}
+
+}
