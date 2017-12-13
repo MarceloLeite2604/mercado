608bf630827baa377217fe70f1587ccc2d4a2b9a
Marcelo De Moraes Leite <marcelo.leite.e@riachuelo.com.br>
2017-12-13 13:52:06 -0200

    Committing changes.

--- mercado-simulator/mercado-simulator/src/main/java/org/marceloleite/mercado/simulator/BuyResponse.java
+++
@@ -1,8 +0,0 @@
-package org.marceloleite.mercado.simulator;
-
-import org.marceloleite.mercado.consumer.model.Currency;
-
-public class BuyResponse {
-
-	
-}
--- mercado-simulator/mercado-simulator/src/main/java/org/marceloleite/mercado/simulator/Main.java
+++ mercado-simulator/mercado-simulator/src/main/java/org/marceloleite/mercado/simulator/Main.java
@@ -1,16 +1,12 @@
 package org.marceloleite.mercado.simulator;
 
-import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.Map;
 
 import org.marceloleite.mercado.commons.util.ObjectToJsonFormatter;
 import org.marceloleite.mercado.commons.util.StringToLocalDateTimeFormatter;
 import org.marceloleite.mercado.consumer.model.Currency;
-import org.marceloleite.mercado.modeler.persistence.TemporalTicker;
-import org.marceloleite.mercado.nnew.TemporalTickerRetriever;
-import org.marceloleite.mercado.simulator.configuration.Configuration;
-import org.marceloleite.mercado.simulator.configuration.Property;
+import org.marceloleite.mercado.nnew.PriceRetriever;
 
 /**
  * Hello world!
@@ -18,57 +14,59 @@ import org.marceloleite.mercado.simulator.configuration.Property;
  */
 public class Main {
 
-	private static final double COMISSION_PERCENTAGE = 0.007;
-
 	public static void main(String[] args) {
-		Configuration configuration = Configuration.getInstance();
 
-		Balance balance = new Balance();
+		Simulator simulator = new Simulator();
+		simulator.getBalance()
+			.deposit(new CurrencyAmount(Currency.REAL, 500.0));
+		simulator.getBalance()
+			.deposit(new CurrencyAmount(Currency.REAL, 1000.0));
 
-		balance = buyFromOtherCurrencyAmount(balance, Currency.BITCOIN, new CurrencyAmount(Currency.REAL, 250.0), new StringToLocalDateTimeFormatter().format("07/09/2017 21:47:15"));
-		balance = buyFromOtherCurrencyAmount(balance, Currency.BITCOIN, new CurrencyAmount(Currency.REAL, 750.0), new StringToLocalDateTimeFormatter().format("29/11/2017 13:32:05"));
-		
-		/*System.out.println(new ObjectToJsonFormatter().format(balance));*/
-		
-		TemporalTicker temporalTicker = retrieveTemporalTicker(Currency.BITCOIN, LocalDateTime.now());
-		System.out.println(balance.getBalances().get(Currency.BITCOIN).getAmount()* temporalTicker.getLast());
+		simulator.buyFromOtherCurrencyAmount(Currency.BITCOIN, new CurrencyAmount(Currency.REAL, 250.0),
+				new StringToLocalDateTimeFormatter().format("07/09/2017 21:47:15"));
+		simulator.buyFromOtherCurrencyAmount(Currency.LITECOIN, new CurrencyAmount(Currency.REAL, 250.0),
+				new StringToLocalDateTimeFormatter().format("07/09/2017 21:47:15"));
+		simulator.buyFromOtherCurrencyAmount(Currency.BITCOIN, new CurrencyAmount(Currency.REAL, 750.0),
+				new StringToLocalDateTimeFormatter().format("29/11/2017 13:32:05"));
+		simulator.buyFromOtherCurrencyAmount(Currency.LITECOIN, new CurrencyAmount(Currency.REAL, 250.0),
+				new StringToLocalDateTimeFormatter().format("29/11/2017 13:32:05"));
 
-	}
+		System.out.println(new ObjectToJsonFormatter().format(simulator.getBalance()));
+		System.out.println(new ObjectToJsonFormatter().format(simulator.getComission()));
 
-	private static Balance buyFromDesiredCurrencyAmount(Balance balance, CurrencyAmount currencyAmountToBuy, LocalDateTime time) {
-		TemporalTicker temporalTicker = retrieveTemporalTicker(currencyAmountToBuy.getCurrency(), time);
-		double cost = currencyAmountToBuy.getAmount() * temporalTicker.getLast();
-		double comission = currencyAmountToBuy.getAmount() * COMISSION_PERCENTAGE;
-		balance.add(Currency.REAL, -cost);
-		balance.add(currencyAmountToBuy.getCurrency(), currencyAmountToBuy.getAmount() - comission);
-		
-		return balance;
-	}
-	
-	private static Balance buyFromOtherCurrencyAmount(Balance balance, Currency desiredCurrency, CurrencyAmount currencyAmountToSpend, LocalDateTime time) {
-		
-		if ( currencyAmountToSpend.getCurrency() != Currency.REAL ) {
-			throw new RuntimeException("Not implemented yet.");
-			
+		Map<Currency, CurrencyAmount> balances = simulator.getBalance()
+			.getBalances();
+
+		System.out.println("Balance: ");
+		for (Currency currency : Currency.values()) {
+			CurrencyAmount currencyAmount = balances.get(currency);
+			if (currencyAmount != null) {
+				calculateRealValueOf(currencyAmount);
+			}
 		}
-		
-		TemporalTicker temporalTicker = retrieveTemporalTicker(desiredCurrency, time);
-		double quantityToBuy = currencyAmountToSpend.getAmount()/temporalTicker.getLast();
-		double comission = quantityToBuy * COMISSION_PERCENTAGE;
-		balance.add(Currency.REAL, -currencyAmountToSpend.getAmount());
-		balance.add(desiredCurrency, quantityToBuy - comission);
-		
-		return balance;
+
+		System.out.println("Comission: ");
+		Map<Currency, CurrencyAmount> comission = simulator.getComission()
+			.getBalances();
+		for (Currency currency : Currency.values()) {
+			CurrencyAmount currencyAmount = comission.get(currency);
+			if (currencyAmount != null) {
+				calculateRealValueOf(currencyAmount);
+			}
+		}
 	}
 
-	private static TemporalTicker retrieveTemporalTicker(Currency currency, LocalDateTime time) {
-		TemporalTickerRetriever temporalTickerRetriever = new TemporalTickerRetriever();
-		Duration duration = Duration.ofSeconds(60);
-		LocalDateTime previousSecond = LocalDateTime.from(time)
-			.minus(duration);
+	private static void calculateRealValueOf(CurrencyAmount currencyAmount) {
+		double currentPrice;
+		if (currencyAmount.getCurrency() != Currency.REAL) {
+			currentPrice = new PriceRetriever().retrieve(currencyAmount.getCurrency(), LocalDateTime.now());
+		} else {
+			currentPrice = 1;
+		}
 
-		List<TemporalTicker> temporalTickers = temporalTickerRetriever.retrieve(currency, previousSecond, time,
-				duration);
-		return temporalTickers.get(0);
+		CurrencyAmount realCurrencyAmount = new CurrencyAmount(Currency.REAL,
+				currencyAmount.getAmount() * currentPrice);
+		System.out.println(realCurrencyAmount);
 	}
+
 }
