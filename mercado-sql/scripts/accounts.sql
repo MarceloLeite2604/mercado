9e5f7d86ae873ee1910a8876321c3490763a1639
MarceloLeite2604 <marceloleite2604@gmail.com>
2018-03-05 18:52:32 -0300

    Committing changes.

--- mercado-sql/scripts/accounts.sql
+++ mercado-sql/scripts/accounts.sql
@@ -1,7 +1,7 @@
 CREATE TABLE accounts (
-    owner   VARCHAR2(64 CHAR) NOT NULL
-)
-LOGGING;
+    owner   VARCHAR2(64 CHAR) NOT NULL,
+    email   VARCHAR2(64 CHAR)
+);
 
 CREATE UNIQUE INDEX acco_owner_idx ON
     accounts ( owner ASC );
