2895dcd41d510a5b262b57e62071b23b9d017b7e
marcelo <marcelo@lapmarcelo>
2018-03-12 12:45:17 -0300

    Committing changes.

---
+++ controller/src/main/java/org/marceloleite/mercado/controller/parameters/ControllerPropertiesRetriever.java
@@ -0,0 +1,50 @@
+package org.marceloleite.mercado.controller.parameters;
+
+import org.marceloleite.mercado.commons.encryption.Encrypt;
+import org.marceloleite.mercado.commons.properties.Property;
+import org.marceloleite.mercado.commons.properties.StandardProperty;
+import org.marceloleite.mercado.retriever.PropertyRetriever;
+
+public class ControllerPropertiesRetriever {
+
+	private static final boolean IGNORE_DATABASE_VALUES = true;
+
+	private static final String PROPERTIES_FILE_NAME = "controller.properties";
+
+	private PropertyRetriever propertyRetriever;
+
+	public ControllerPropertiesRetriever() {
+		this.propertyRetriever = new PropertyRetriever(PROPERTIES_FILE_NAME);
+	}
+
+	private Property retrieveProperty(ControllerProperty controllerProperty) {
+		StandardProperty standardProperty = propertyRetriever.retrieve(controllerProperty, IGNORE_DATABASE_VALUES);
+		if (standardProperty == null) {
+			if (controllerProperty.isRequired()) {
+				throw new RuntimeException("Could not find property \"" + controllerProperty.getName() + "\".");
+			} else {
+				return new StandardProperty(controllerProperty.getName(), controllerProperty.getDefaultValue(),
+						controllerProperty.isRequired());
+			}
+
+		} else {
+			if (controllerProperty.isEncrypted()) {
+				String decryptedValue = new Encrypt().decrypt(standardProperty.getValue());
+				return new StandardProperty(controllerProperty.getName(), decryptedValue,
+						controllerProperty.isRequired());
+			} else {
+				return standardProperty;
+			}
+		}
+
+	}
+
+	public String retrievePersistenceFileName() {
+		return retrieveProperty(ControllerProperty.PERSISTENCE_FILE).getValue();
+	}
+	
+	public String retrieveXmlDirectoryPath() {
+		return retrieveProperty(ControllerProperty.XML_DIRECTORY_PATH).getValue();
+	}
+
+}
---
+++ controller/src/main/java/org/marceloleite/mercado/controller/parameters/ControllerProperty.java
@@ -0,0 +1,64 @@
+package org.marceloleite.mercado.controller.parameters;
+
+import org.marceloleite.mercado.commons.properties.Property;
+
+public enum ControllerProperty implements Property {
+
+	PERSISTENCE_FILE("controller.persistenceFile", false, false, "persistence.properties"),
+	XML_DIRECTORY_PATH("controller.xmlDirectoryPath", true, false);
+	
+	private ControllerProperty(String name, boolean required, boolean encrypted, String defaultValue) {
+		this.name = name;
+		this.required = required;
+		this.encrypted = encrypted;
+		this.defaultValue = defaultValue;
+	}
+
+	private ControllerProperty(String name, boolean required, boolean encrypted) {
+		this(name, required, encrypted, null);
+	}
+
+	private String name;
+
+	private String value;
+	
+	private String defaultValue;
+
+	private boolean required;
+
+	private boolean encrypted;
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public void setName(String name) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public String getValue() {
+		return value;
+	}
+
+	@Override
+	public void setValue(String value) {
+		this.value = value;
+
+	}
+
+	@Override
+	public boolean isRequired() {
+		return required;
+	}
+
+	public boolean isEncrypted() {
+		return encrypted;
+	}
+	
+	public String getDefaultValue() {
+		return defaultValue;
+	}
+}
